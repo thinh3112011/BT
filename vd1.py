998aa263adb32b4b27fc2a3e3e4dee03de5d759a
unknown <thinh.21032022@gmail.com>
2025-06-12 22:39:28 +0700

    hello

---
+++ vd1.py
@@ -0,0 +1,24 @@
+with open("input.txt","r") as fi:
+    n = list(fi.read().split())
+print(n)   
+m = []
+a = 0
+for i in n:
+    if i.isdigit():
+        dem = m[a:n.ing(i)]
+        m.append(dem) 
+        #a = n[int(n.index(i))+1]
+print(m)
+    
+    
+    #n = fi.read()
+#print(n)
+#m = []
+#for i in n:
+    #print(i)
+ #   if i.isdigit():
+  #      m.append(int(i))
+#print(m)
+#a = m.index(max(m))+1
+#print(a)
+
